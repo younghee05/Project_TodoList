bb35232b4966d2dd2cd3cb03efa5a372915c0ab4
younghee05 <k1437013@gmail.com>
2024-07-29 10:22:00 +0900

    todolist 틀

---
+++ todolist_front/src/components/Container/Container.jsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import * as s from "./style";
+
+import React from 'react';
+
+function Container(props) {
+    return (
+        <div css={s.container}>
+            
+        </div>
+    );
+}
+
+export default Container;
---
+++ todolist_front/src/pages/style.js
@@ -0,0 +1,17 @@
+import { css } from "@emotion/react";
+
+export const mainlayout = css`
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const layout = css`
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    margin: 20px;
+
+    
+`;
